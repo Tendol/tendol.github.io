a3f023c72e894053f53ac04b8dc9ccde93ffa155
Tenzin Dolma Gyalpo <tenzindolmagyalpo@Tenzins-MacBook-Pro.local>
2020-08-02 17:51:37 -0700

    Updates

---
+++ precache-manifest.323a69e2796ca917e7ff5dddf06e3910.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09181529e3596ff29755bd763530c02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ddff3d3a2d8ee1af00d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b1b458987885d52e001",
+    "url": "/static/js/2.e4ed184d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4ed184d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ddff3d3a2d8ee1af00d",
+    "url": "/static/js/main.bbced441.chunk.js"
+  },
+  {
+    "revision": "c1123a04b8bcd05fa02c",
+    "url": "/static/js/runtime-main.d5f4d8d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
